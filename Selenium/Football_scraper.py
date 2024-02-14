ca90b418a83c2093b59e1df652ae741f639c24c4
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2024-02-14 00:07:51 -0500

    docs: rename and add comment in Football score scraper

--- Selenium/Football_scraper.py
+++ Selenium/Football_scraper.py
@@ -1,30 +1,44 @@
+# Import necessary libraries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
+# Define the website URL
 website = 'https://www.adamchoi.co.uk/overs/detailed'
+
+# Initialize the Chrome webdriver
 driver = webdriver.Chrome()
 
+# Open the website in the browser
 driver.get(website)
 
+# Click on the 'All matches' button
 all_match_button = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']")
 all_match_button.click()
 
+# Find all elements with tag 'tr' (table rows) representing matches
 matches = driver.find_elements(By.TAG_NAME, 'tr')
 
+# Initialize lists to store match data
 date = []
 home_team = []
 score = []
 away_team = []
+
+# Iterate over each match row to extract data
 for match in matches:
-    # index starts from 1 in xpath
+    # Extract data from each column (td) of the current match row
     date.append(match.find_element(By.XPATH, './td[1]').text)
     home_team.append(match.find_element(By.XPATH, './td[2]').text)
     score.append(match.find_element(By.XPATH, './td[3]').text)
     away_team.append(match.find_element(By.XPATH, './td[4]').text)
 
+# Create a DataFrame from the extracted data
 df = pd.DataFrame({'Date': date, "Home team": home_team, 'Score': score, 'Away team': away_team})
+
+# Save the DataFrame to a CSV file
 df.to_csv('Scraped_data/football_pl_data.csv', index=False)
 
+# Close the webdriver
 driver.quit()
