32f1bb4b5bc1e21c3bf15da838a0fe0ced46e5ae
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2024-02-20 01:50:28 -0500

    docs: add code comment

--- Projects/Indeed Scraper/Scraper.py
+++ Projects/Indeed Scraper/Scraper.py
@@ -16,6 +16,7 @@ container = driver.find_element(By.ID, 'mosaic-jobResults')
 jobs = container.find_elements(By.XPATH, ".//div[contains(@class, 'job_seen_beacon')]")
 print(len(jobs))
 wait = WebDriverWait(driver, 10)
+# Initialize arrays to store job-related data
 title_array = []
 company_array = []
 location_array = []
@@ -26,87 +27,94 @@ pay_array = []
 job_type_array = []
 shift_array = []
 
+# Loop through each job
 for job in jobs:
-    print("-----------------------------------------------------------------------------------------------")
-
-    # Scroll to the job element (if needed)
+    # Scroll to the job element and click it
+    # (Assuming 'driver' is an instance of WebDriver)
+    # This allows us to access the details of each job
     driver.execute_script("arguments[0].scrollIntoView();", job)
-    # Click the job element
     child_tag = job.find_element(By.TAG_NAME, 'a')
     child_tag.click()
     time.sleep(2)
 
+    # Find the container for extra job data
     extra_data_container = driver.find_element(By.XPATH, ".//div[contains(@class, 'jobsearch-JobComponent')]")
-    # try:
+
+    # Extract job title and append it to the title array
     title = extra_data_container.find_element(By.XPATH,
                                               ".//h2[contains(@class, 'jobsearch-JobInfoHeader-title')]").text
+    title_array.append(title.split("-")[0].strip())
 
-    if title:
-        title_array.append(title.split("-")[0].strip())
+    # Extract company name or NaN if not found, and append it to the company array
+    try:
+        company = extra_data_container.find_element(By.XPATH, "//div[@data-testid='inlineHeader-companyName']").text
+    except NoSuchElementException:
+        company = np.nan
+    company_array.append(company)
 
-        try:
-            company = extra_data_container.find_element(By.XPATH, "//div[@data-testid='inlineHeader-companyName']").text
-        except NoSuchElementException:
-            company = np.nan
-        company_array.append(company)
+    # Extract job details section
+    job_details = extra_data_container.find_elements(By.XPATH,
+                                                     "//div[contains(@class, 'js-match-insights-provider-16m282m')]")
 
-        job_details = extra_data_container.find_elements(By.XPATH,
-                                                         "//div[contains(@class, 'js-match-insights-provider-16m282m')]")
+    # Initialize flags to track whether data has been appended for each job detail
+    appended_pay = False
+    appended_job_type = False
+    appended_shift = False
 
-        # Initialize flags for tracking whether data has been appended for each job
-        appended_pay = False
-        appended_job_type = False
-        appended_shift = False
+    # Loop through job details
+    for job_detail in job_details:
+        # Extract title and text body for each job detail
+        data_head = job_detail.find_element(By.XPATH,
+                                            ".//div[contains(@class, 'js-match-insights-provider-e6s05i')]")
+        title = data_head.find_element(By.TAG_NAME, "h3").text.lower().replace(" ", '_')
+        text_body = data_head.find_elements(By.TAG_NAME, 'li')
+        final_text = ', '.join(map(lambda x: x.text, text_body))
 
-        for job_detail in job_details:
-            data_head = job_detail.find_element(By.XPATH,
-                                                ".//div[contains(@class, 'js-match-insights-provider-e6s05i')]")
+        # Append data to the appropriate array based on title
+        if title == 'pay' and not appended_pay:
+            pay_array.append(final_text)
+            appended_pay = True
+        elif title == 'job_type' and not appended_job_type:
+            job_type_array.append(final_text)
+            appended_job_type = True
+        elif title == 'shift_and_schedule' and not appended_shift:
+            shift_array.append(final_text)
+            appended_shift = True
 
-            title = data_head.find_element(By.TAG_NAME, "h3").text.lower().replace(" ", '_')
-            text_body = data_head.find_elements(By.TAG_NAME, 'li')
-            final_text = ', '.join(map(lambda x: x.text, text_body))
+    # If data is not appended for pay_array, job_type_array, or shift_array, append NaN
+    if not appended_pay:
+        pay_array.append(np.nan)
+    if not appended_job_type:
+        job_type_array.append(np.nan)
+    if not appended_shift:
+        shift_array.append(np.nan)
 
-            if title == 'pay' and not appended_pay:
-                pay_array.append(final_text)
-                appended_pay = True
-            elif title == 'job_type' and not appended_job_type:
-                job_type_array.append(final_text)
-                appended_job_type = True
-            elif title == 'shift_and_schedule' and not appended_shift:
-                shift_array.append(final_text)
-                appended_shift = True
+    # Extract job location or NaN if not found, and append it to the location array
+    try:
+        location = extra_data_container.find_element(By.XPATH, "//div[@data-testid='job-location']").text
+    except NoSuchElementException:
+        location = np.nan
+    location_array.append(location)
 
-        # If data is not appended for pay_array, append NaN
-        if not appended_pay:
-            pay_array.append(np.nan)
-        # If data is not appended for job_type_array, append NaN
-        if not appended_job_type:
-            job_type_array.append(np.nan)
-        # If data is not appended for shift_array, append NaN
-        if not appended_shift:
-            shift_array.append(np.nan)
+    # Extract job description or NaN if not found, and append it to the description array
+    try:
+        description = extra_data_container.find_element(By.ID, "jobDescriptionText").text.strip()
+    except NoSuchElementException:
+        description = np.nan
+    description_array.append(description)
 
-        try:
-            location = extra_data_container.find_element(By.XPATH, "//div[@data-testid='job-location']").text
-        except NoSuchElementException:
-            location = np.nan
-        location_array.append(location)
+    # Extract job benefits or NaN if not found, and append it to the benefit array
+    try:
+        benefits = extra_data_container.find_element(By.ID, 'benefits')
+        li_texts = ', '.join(map(lambda li: li.text, benefits.find_elements(By.TAG_NAME, 'li')))
+    except NoSuchElementException:
+        li_texts = np.nan
+    benefit_array.append(li_texts)
 
-        try:
-            description = extra_data_container.find_element(By.ID, "jobDescriptionText").text.strip()
-        except NoSuchElementException:
-            description = np.nan
-        description_array.append(description)
-
-        try:
-            benefits = extra_data_container.find_element(By.ID, 'benefits')
-            li_texts = ', '.join(map(lambda li: li.text, benefits.find_elements(By.TAG_NAME, 'li')))
-        except NoSuchElementException:
-            li_texts = np.nan
-        benefit_array.append(li_texts)
-
+# Sleep to ensure all data is loaded before quitting the driver
 time.sleep(7)
 
+# Quit the WebDriver
 driver.quit()
 
 # Print the length of each array
